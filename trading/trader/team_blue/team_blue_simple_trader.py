8a9563f8048da26a2b3805920ed70aca09af89cd
mrode <mateusz.rode@outlook.com>
2018-10-25 11:53:42 +0200

    Task 0 blue

--- trading/trader/team_blue/team_blue_simple_trader.py
+++ trading/trader/team_blue/team_blue_simple_trader.py
@@ -3,6 +3,9 @@ Created on 08.11.2017
 
 @author: jtymoszuk
 """
+import math
+
+from model.CompanyEnum import CompanyEnum
 from model.Portfolio import Portfolio
 from model.StockMarketData import StockMarketData
 from model.ITrader import ITrader
@@ -36,7 +39,42 @@ class TeamBlueSimpleTrader(ITrader):
           A OrderList instance, may be empty never None
         """
 
+        y_a = stock_market_data.get_most_recent_price(CompanyEnum.COMPANY_A)
+        y_b = stock_market_data.get_most_recent_price(CompanyEnum.COMPANY_B)
+
+        p_a = self.stock_a_predictor.doPredict(stock_market_data[CompanyEnum.COMPANY_A]);
+        p_b = self.stock_b_predictor.doPredict(stock_market_data[CompanyEnum.COMPANY_B]);
+
+        r_a = (p_a - y_a) / y_a * 100;
+        r_b = (p_b - y_b) / y_b * 100;
+
         result = OrderList()
+
+        if (r_a < 0 and portfolio.get_amount(CompanyEnum.COMPANY_A) > 0):
+            result.sell(CompanyEnum.COMPANY_A, portfolio.get_amount(CompanyEnum.COMPANY_A))
+
+        if (r_b < 0 and portfolio.get_amount(CompanyEnum.COMPANY_B) > 0):
+            result.sell(CompanyEnum.COMPANY_B, portfolio.get_amount(CompanyEnum.COMPANY_B))
+
+
+        if (r_a <= 0 and r_b <= 0):
+            return result
+
+
+        company = CompanyEnum.COMPANY_A
+
+        if (r_b > r_a):
+            company = CompanyEnum.COMPANY_B
+
+        buy_amount = math.floor(portfolio.cash / stock_market_data.get_most_recent_price(company))
+
+        if (portfolio.cash > 0 and buy_amount > 0):
+            result.buy(company, buy_amount)
+
+
+        print("Portfolio: " + str(portfolio.total_value(stock_market_data.get_most_recent_trade_day(), stock_market_data)));
+
+
 
         # TODO: implement trading logic
 
